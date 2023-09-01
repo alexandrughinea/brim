2582a1ec74399bdabca4c82ded07b0f1252b42c5
Alex G <>
2023-09-01 19:39:44 +0300

    Expose utiltities

--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -3,9 +3,15 @@ pub mod brew_formatting;
 pub mod brew_install_packages;
 pub mod brew_list_installed_packages;
 pub mod brew_remove_packages;
+pub mod urls;
+pub mod file;
 
 pub use brew_fetch_packages::fetch_packages;
 pub use brew_formatting::format_package_name;
 pub use brew_install_packages::install_packages;
 pub use brew_list_installed_packages::list_installed_packages;
 pub use brew_remove_packages::remove_packages;
+
+pub use file::get_local_file_path_content;
+
+pub use urls::parse_file_url_list;
